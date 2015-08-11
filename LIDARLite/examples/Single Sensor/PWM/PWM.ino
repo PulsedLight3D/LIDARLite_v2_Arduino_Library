5302c7eb4c309de537fb12624a40ac775b0cf782
Austin Meyers <am@ak5a.com>
2015-08-11 16:38:01 -0700

    Added PWM example and updated PWM diagram to include the 680uF capacitor

--- LIDARLite/examples/Single Sensor/PWM/PWM.ino
+++ LIDARLite/examples/Single Sensor/PWM/PWM.ino
@@ -3,7 +3,7 @@
 
   This example demonstrates how to read measurements from LIDAR-Lite v2 "Blue
   Label" using PWM
-  
+
   The library is in BETA, so subscribe to the github repo to recieve updates, or
   just check in periodically:
   https://github.com/PulsedLight3D/LIDARLite_v2_Arduino_Library
@@ -11,12 +11,21 @@
   To learn more read over lidarlite.cpp as each function is commented
 =========================================================================== */
 
-void setup() {
-  // put your setup code here, to run once:
+unsigned long pulse_width;
 
+void setup()
+{
+  Serial.begin(115200); // Start serial communications
+  pinMode(2, OUTPUT); // Set pin 2 as trigger pin
+  pinMode(3, INPUT); // Set pin 3 as monitor pin
+  digitalWrite(2, LOW); // Set trigger LOW for continuous read
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  pulse_width = pulseIn(3, HIGH); // Count how long the pulse is high in microseconds
+  if(pulse_width != 0){ // If we get a reading that isn't zero, let's print it
+        pulse_width = pulse_width/10; // 10usec = 1 cm of distance for LIDAR-Lite
+  	Serial.println(pulse_width); // Print the distance
+  }
 }
